42b93a6adfff4b4734e0d4d10fea4770ca0d026a
Rishugeek <143407316+Rishugeek@users.noreply.github.com>
2024-03-02 23:52:38 +0530

    UI CHANGES

--- js files/script.js
+++ js files/script.js
@@ -20,6 +20,7 @@ function showSlides() {
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
+
 // Core Team Swiper Code
 // ⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡//
 var swiper = new Swiper(".mySwiper", {
@@ -39,27 +40,4 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
-// Scroll to Top Button Script 🎇🎇
-
-// Get the button
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// Core Team Swiper Code
